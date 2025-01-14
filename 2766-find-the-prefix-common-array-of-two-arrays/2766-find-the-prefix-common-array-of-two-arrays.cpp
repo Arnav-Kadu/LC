7284b0277e2ef0926f26ffcefe6c57cc590f5c71
Arnav-Kadu <105575763+Arnav-Kadu@users.noreply.github.com>
2025-01-14 10:51:08 +0530

    Time: 8 ms (53.56%), Space: 86.3 MB (65.66%) - LeetHub

---
+++ 2766-find-the-prefix-common-array-of-two-arrays/2766-find-the-prefix-common-array-of-two-arrays.cpp
@@ -0,0 +1,38 @@
+class Solution {
+private:
+    void update(vector<int>& bit, int idx, int val, int n) {
+        while (idx <= n) {
+            bit[idx] += val;
+            idx += (idx & -idx);
+        }
+    }
+    int query(vector<int>& bit, int idx) {
+        int sum = 0;
+        while (idx > 0) {
+            sum += bit[idx];
+            idx -= (idx & -idx);
+        }
+        return sum;
+    }
+
+public:
+    vector<int> findThePrefixCommonArray(vector<int>& A, vector<int>& B) {
+        int n = A.size();
+        vector<int> ans;
+        vector<int> bit(n + 1, 0);
+        vector<bool> seen(n + 1, false); 
+        for (int i = 0; i < n; i++) {
+            if (seen[A[i]]) {
+                update(bit, A[i], 1, n);  
+            }
+            seen[A[i]] = true;
+            if (seen[B[i]]) {
+                update(bit, B[i], 1, n);  
+            }
+            seen[B[i]] = true;
+            ans.push_back(query(bit, n));
+        }
+        
+        return ans;
+    }
+};
