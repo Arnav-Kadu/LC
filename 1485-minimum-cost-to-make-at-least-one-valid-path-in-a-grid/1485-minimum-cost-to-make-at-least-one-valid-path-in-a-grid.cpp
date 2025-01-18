f3fd47f27b80a316e25e957bc70ad89accb824b2
Arnav-Kadu <105575763+Arnav-Kadu@users.noreply.github.com>
2025-01-18 11:40:49 +0530

    Time: 25 ms (60.34%), Space: 18.2 MB (85.37%) - LeetHub

--- 1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid.cpp
+++ 1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid.cpp
@@ -4,7 +4,7 @@ public:
         int n = grid.size();
         int m = grid[0].size();
         vector<vector<int>> dis(n, vector<int>(m, INT_MAX));
-        priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<pair<int, pair<int, int>>>> pq;
+        priority_queue<pair<int, pair<int, int>>,vector<pair<int, pair<int, int>>>,greater<pair<int, pair<int, int>>>>pq;
         vector<int> dx = {0, 0, 1, -1};
         vector<int> dy = {1, -1, 0, 0};
 
@@ -12,25 +12,29 @@ public:
         dis[0][0] = 0;
 
         while (!pq.empty()) {
-            auto [cost, pos] = pq.top();
-            int x = pos.first;
-            int y = pos.second;
+            auto top = pq.top();
+            int cost = top.first;
+            int x = top.second.first;
+            int y = top.second.second;
             pq.pop();
 
-            if (cost > dis[x][y]) continue;
-
+            if (cost > dis[x][y])
+                continue;
+            if (x == n - 1 && y == m - 1)
+                return cost;
             for (int i = 0; i < 4; ++i) {
                 int nx = x + dx[i];
                 int ny = y + dy[i];
                 int nc = cost + (grid[x][y] == i + 1 ? 0 : 1);
 
-                if (nx >= 0 && ny >= 0 && nx < n && ny < m && nc < dis[nx][ny]) {
+                if (nx >= 0 && ny >= 0 && nx < n && ny < m &&
+                    nc < dis[nx][ny]) {
                     dis[nx][ny] = nc;
                     pq.push({nc, {nx, ny}});
                 }
             }
         }
-        
+
         return dis[n - 1][m - 1];
     }
 };
