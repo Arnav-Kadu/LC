865655c5ba6bf4fcef7fd9c1c5ef2fb7d63beef5
Arnav-Kadu <105575763+Arnav-Kadu@users.noreply.github.com>
2025-01-25 13:01:45 +0530

    Time: 78 ms (95.27%), Space: 131.2 MB (94.59%) - LeetHub

--- 3219-make-lexicographically-smallest-array-by-swapping-elements/3219-make-lexicographically-smallest-array-by-swapping-elements.cpp
+++ 3219-make-lexicographically-smallest-array-by-swapping-elements/3219-make-lexicographically-smallest-array-by-swapping-elements.cpp
@@ -1,39 +1,30 @@
 class Solution {
 public:
-    vector<int> lexicographicallySmallestArray(vector<int>& arr, int threshold) {
-        vector<pair<int, int>> valueIndexPairs;
-        int size = arr.size();
+    vector<int> lexicographicallySmallestArray(vector<int>& nums, int limit) {
+        int n = nums.size();
+        vector<pair<int,int>> vp(n);
+        for(int i=0;i<n;i++)vp[i]={nums[i],i};
 
-        for (int i = 0; i < size; ++i) {
-            valueIndexPairs.push_back({arr[i], i});
-        }
+        sort(vp.begin(),vp.end());
+        vector<int> ind;
+        ind.push_back(vp[0].second);
+        int prv=0;
+        for(int i=1;i<n;i++){
+            if(abs(vp[i].first-vp[i-1].first)<=limit){
+                ind.push_back(vp[i].second);
+            }
+            else{
+                sort(ind.begin()+prv,ind.end());
+                prv=i;
+                ind.push_back(vp[i].second);
 
-        sort(valueIndexPairs.begin(), valueIndexPairs.end());
-
-        vector<vector<pair<int, int>>> groupedPairs;
-        groupedPairs.push_back({valueIndexPairs[0]});
-
-        for (int i = 1; i < size; ++i) {
-            if (valueIndexPairs[i].first - valueIndexPairs[i - 1].first <= threshold) {
-                groupedPairs.back().push_back(valueIndexPairs[i]);
-            } else {
-                groupedPairs.push_back({valueIndexPairs[i]});
             }
         }
+        sort(ind.begin()+prv,ind.end());
 
-        for (const auto& group : groupedPairs) {
-            vector<int> indices;
-            for (const auto& [value, index] : group) {
-                indices.push_back(index);
-            }
-
-            sort(indices.begin(), indices.end());
-
-            for (size_t i = 0; i < indices.size(); ++i) {
-                arr[indices[i]] = group[i].first;
-            }
+        for(int i=0;i<n;i++){
+            nums[ind[i]]=vp[i].first;
         }
-
-        return arr;
+        return nums;
     }
 };
