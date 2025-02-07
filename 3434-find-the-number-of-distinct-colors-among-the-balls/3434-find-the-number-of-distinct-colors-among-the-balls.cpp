f40feaa40dd593e3defb09956d4b01de5049d467
Arnav-Kadu <105575763+Arnav-Kadu@users.noreply.github.com>
2025-02-07 12:00:04 +0530

    Time: 142 ms (41.16%), Space: 158.5 MB (41.8%) - LeetHub

--- 3434-find-the-number-of-distinct-colors-among-the-balls/3434-find-the-number-of-distinct-colors-among-the-balls.cpp
+++ 3434-find-the-number-of-distinct-colors-among-the-balls/3434-find-the-number-of-distinct-colors-among-the-balls.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     vector<int> queryResults(int limit, vector<vector<int>>& queries) {
-        set<int> s;
-        unordered_map<int, int> cnt;
+        map<int, int> cnt;
         unordered_map<int, int> v;
+        int distinct_colors = 0;
         vector<int> ans;
 
         for (auto& query : queries) {
@@ -11,14 +11,13 @@ public:
 
             if (v.count(x)) {
                 cnt[v[x]]--;
-                if (cnt[v[x]] == 0) s.erase(v[x]);
+                if (cnt[v[x]] == 0) distinct_colors--;
             }
 
             v[x] = y;
-            cnt[y]++;
-            s.insert(y);
-            
-            ans.push_back(s.size());
+            if (++cnt[y] == 1) distinct_colors++;
+
+            ans.push_back(distinct_colors);
         }
 
         return ans;
