c11dc1c04c8f2b4be755802c536671c09550d9ab
Arnav-Kadu <105575763+Arnav-Kadu@users.noreply.github.com>
2025-02-09 11:33:18 +0530

    Time: 30 ms (91.64%), Space: 70.5 MB (90.43%) - LeetHub

--- 2448-count-number-of-bad-pairs/2448-count-number-of-bad-pairs.cpp
+++ 2448-count-number-of-bad-pairs/2448-count-number-of-bad-pairs.cpp
@@ -1,28 +1,35 @@
 class Solution {
 public:
     long long countBadPairs(vector<int>& nums) {
-        //j-i!=nums[j]-nums[i]
-        // j-nums[j]-i+nums[i]!=0
-        //j-nums[j]-(i-nums[i])!=0
-        //diff[j]-diff[i]!=0
+        // j-i != nums[j]-nums[i]
+        // j-nums[j]-i+nums[i] != 0
+        // j-nums[j]-(i-nums[i]) != 0
+        // diff[j]-diff[i] != 0
         // multiply by -1
-        //diff[i]-diff[j]!=0 
+        // diff[i]-diff[j] != 0 
         // that means order doesn't matter
-        //diff array bana ke solve kr lo 
-        int n=nums.size();
-        vector<int>diff(n);
-        for(int i=0;i<n;i++){
-            diff[i]=(i-nums[i]);
-        }
-        unordered_map<int, long long> freq;
-        long long count = 0;
+        // diff array bana ke solve kr lo 
         
+        int n = nums.size();
+        vector<int> diff(n);
         for (int i = 0; i < n; i++) {
-            count += freq[diff[i]];
-            freq[diff[i]]++;
+            diff[i] = i - nums[i];
         }
+        
+        sort(diff.begin(), diff.end());
+        
+        long long good = 0, count = 1;
+        for (int i = 1; i < n; i++) {
+            if (diff[i] == diff[i - 1]) {
+                count++;
+            } else {
+                good += (count * (count - 1)) / 2;
+                count = 1;
+            }
+        }
+        good += (count * (count - 1)) / 2;
         
         long long all = (1LL * n * (n - 1)) / 2;
-        return all - count;
+        return all - good;
     }
 };
