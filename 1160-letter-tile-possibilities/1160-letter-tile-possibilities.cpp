3caacd8f82f28a3d1086c682029d7ed05bc2471e
Arnav-Kadu <105575763+Arnav-Kadu@users.noreply.github.com>
2025-02-17 13:21:14 +0530

    Time: 2 ms (85.65%), Space: 8.6 MB (69.21%) - LeetHub

--- 1160-letter-tile-possibilities/1160-letter-tile-possibilities.cpp
+++ 1160-letter-tile-possibilities/1160-letter-tile-possibilities.cpp
@@ -1,23 +1,35 @@
 class Solution {
 public:
-    void backtrack(map<char, int>& m, int& ans) {
-        for (auto &i : m) {
-            if (i.second> 0) {
-                ans++; 
-                i.second--; 
-                backtrack(m, ans);
-                i.second++; 
-            }
-        }
-    }
-
     int numTilePossibilities(string tiles) {
-        map<char, int> m;
-        for (char ch : tiles) {
-            m[ch]++;
-        }
-        int ans = 0;
-        backtrack(m, ans);
+        unordered_map<char, int> freq;
+        for (char c : tiles)
+            freq[c]++;
+        vector<int> counts;
+        for (auto& p : freq)
+            counts.push_back(p.second);
+        int n = tiles.size();
+        vector<long long> fact(n + 1, 1);
+        for (int i = 1; i <= n; i++)
+            fact[i] = fact[i - 1] * i;
+        long long ans = 0;
+        int m = counts.size();
+        vector<int> current(m, 0);
+        function<void(int, int)> dfs = [&](int pos, int sum) {
+            if (pos == m) {
+                if (sum > 0) {
+                    long long ways = fact[sum];
+                    for (int i = 0; i < m; i++)
+                        ways /= fact[current[i]];
+                    ans += ways;
+                }
+                return;
+            }
+            for (int i = 0; i <= counts[pos]; i++) {
+                current[pos] = i;
+                dfs(pos + 1, sum + i);
+            }
+        };
+        dfs(0, 0);
         return ans;
     }
 };
