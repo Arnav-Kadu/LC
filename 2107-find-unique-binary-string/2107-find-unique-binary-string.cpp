32b5ba2c8a75b4e3ca9c29499052cccf6e2af7ff
Arnav-Kadu <105575763+Arnav-Kadu@users.noreply.github.com>
2025-02-20 06:47:39 +0530

    Time: 0 ms (100%), Space: 12.6 MB (84%) - LeetHub

--- 2107-find-unique-binary-string/2107-find-unique-binary-string.cpp
+++ 2107-find-unique-binary-string/2107-find-unique-binary-string.cpp
@@ -1,29 +1,12 @@
 class Solution {
-private:
-    void generate(vector<int>& set, unordered_map<string, int>& powerSetMap) {
-        int n = set.size();
-        int totalSubsets = (1 << n);
-        for (int mask = 0; mask < totalSubsets; ++mask) {
-            string binaryKey = bitset<16>(mask).to_string().substr(16 - n);                         
-            powerSetMap[binaryKey] = 1;
-        }
-    }
-
 public:
     string findDifferentBinaryString(vector<string>& nums) {
         int n = nums.size();
-        vector<int> set(n);
-        iota(set.begin(), set.end(), 1);
-        unordered_map<string, int> mapper;
-        generate(set, mapper);
-        for (auto i : nums) {
-            mapper[i] = 0;
+        string ans(n, ' ');
+        // if we think properly only 1 bit should differ from each element in nums thats our answer
+        for (int i = 0; i < n; ++i) {
+            ans[i] = (nums[i][i] == '0' ? '1' : '0');
         }
-        for (auto i : mapper) {
-            if (i.second == 1) {
-                return i.first;
-            }
-        }
-        return "";
+        return ans;
     }
 };
