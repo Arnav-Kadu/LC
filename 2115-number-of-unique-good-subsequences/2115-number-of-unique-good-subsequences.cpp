22f8cdb8a3330eb6c86a842be7ecc53c0cdc133c
Arnav-Kadu <105575763+Arnav-Kadu@users.noreply.github.com>
2025-03-01 23:27:58 +0530

    Time: 10 ms (31.61%), Space: 25.8 MB (13.55%) - LeetHub

--- 2115-number-of-unique-good-subsequences/2115-number-of-unique-good-subsequences.cpp
+++ 2115-number-of-unique-good-subsequences/2115-number-of-unique-good-subsequences.cpp
@@ -1,24 +1,36 @@
 class Solution {
 public:
     int numberOfUniqueGoodSubsequences(string s) {
-        const long long mod = 1e9 + 7;
-        if(s.find('1') == string::npos) return 1;
-        int pos = s.find('1');
-        string t = s.substr(pos);
-        int L = t.size() - 1;
-        vector<long long> dp(L + 1, 0);
-        dp[0] = 1;
-        int last[2] = {-1, -1};
-        for (int i = 0; i < L; i++) {
-            int c = t[i + 1] - '0';
-            long long cur = (dp[i] * 2LL) % mod;
-            if(last[c] != -1)
-                cur = (cur - dp[last[c]] + mod) % mod;
-            dp[i + 1] = cur;
-            last[c] = i;
+        // dp[i]=2*dp[i-1]-dupilicate
+        int mod=1e9+7;
+        int first=-1;
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='1'){
+                first=i;
+                break;
+            }
         }
-        long long validOnes = dp[L] % mod;
-        bool hasZero = (s.find('0') != string::npos);
-        return (validOnes + (hasZero ? 1 : 0)) % mod;
+        if(first==-1){
+            return 1;
+        }
+        vector<long long>dp(s.size());
+        dp[first]=1;
+        int lastzero=0,lastone=0;
+        for(int i=first+1;i<s.size();i++){
+           int j=s[i]=='0'?lastzero:lastone;
+           int dup=0;
+           if(j>0){
+            dup=dp[j-1];
+           }
+           
+           dp[i]=(2LL*dp[i-1]-dup)%mod;
+           if (dp[i] < 0) dp[i] +=mod;
+           if(s[i]=='0') lastzero=i;
+           else lastone=i;
+        }
+        int zero=0;
+        if(s.find('0') != string::npos) zero=1;
+        
+        return (dp[s.size()-1]+zero)%mod;
     }
 };
