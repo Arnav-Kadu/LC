8c2418faaa321d5ef71b6b5e1f445c2fe972106a
Arnav-Kadu <105575763+Arnav-Kadu@users.noreply.github.com>
2025-03-05 11:19:45 +0530

    Time: 0 ms (100%), Space: 7.9 MB (61.42%) - LeetHub

--- 2649-count-total-number-of-colored-cells/2649-count-total-number-of-colored-cells.cpp
+++ 2649-count-total-number-of-colored-cells/2649-count-total-number-of-colored-cells.cpp
@@ -3,9 +3,11 @@ public:
     long long coloredCells(int n) {
         long long ans=1;
         n--;
-        for(int i=1;i<=n;i++){
-            ans+=(i*4);
-        }
+        // for(int i=1;i<=n;i++){
+        //     ans+=(i*4);
+        // }
+        // n(n+1)/2 * 4
+        ans+=(long long)(n*(long long)(n+1)/2)*4;
         return ans;
     }
 };
