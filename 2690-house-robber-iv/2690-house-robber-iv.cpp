61e318e572a68510311a7cdda7b79596c9edfa0e
Arnav-Kadu <105575763+Arnav-Kadu@users.noreply.github.com>
2025-03-15 10:56:44 +0530

    Time: 21 ms (70.94%), Space: 60.8 MB (67.69%) - LeetHub

---
+++ 2690-house-robber-iv/2690-house-robber-iv.cpp
@@ -0,0 +1,30 @@
+class Solution {
+private:
+    bool check(int mid,int k,vector<int>&nums){
+        int see=0;
+        for(int i=0;i<nums.size();i++){
+            if(mid>=nums[i]){
+                see++;
+                i++;
+            }
+        }
+        return see>=k;
+    }
+public:
+    int minCapability(vector<int>& nums, int k) {
+        int low=*min_element(nums.begin(),nums.end());
+        int high=*max_element(nums.begin(),nums.end());
+        int n=nums.size();
+        int ans=0;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(check(mid,k,nums)){
+                ans=mid;
+                high=mid-1;
+            }else{
+                low=mid+1;
+            }
+        }
+        return ans;
+    }
+};
