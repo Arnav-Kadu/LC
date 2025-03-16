652ff42f9c75b945570b0d7d90b655b4a55f5ed5
Arnav-Kadu <105575763+Arnav-Kadu@users.noreply.github.com>
2025-03-16 11:10:25 +0530

    Time: 11 ms (94.54%), Space: 60.2 MB (5.84%) - LeetHub

--- 2665-minimum-time-to-repair-cars/2665-minimum-time-to-repair-cars.cpp
+++ 2665-minimum-time-to-repair-cars/2665-minimum-time-to-repair-cars.cpp
@@ -1,25 +1,32 @@
 class Solution {
 private:
-    bool check(long long mid, vector<int>& ranks, int cars) {
+    bool check(long long mid, const unordered_map<int, int>& freq, int cars) {
         long long total = 0;
-        for (int r : ranks) {
-            total += floor(sqrt((long double)mid / r));
+        for (auto &p : freq) {
+            int r = p.first, cnt = p.second;
+            total += cnt * (long long)floor(sqrt((long double)mid / r));
+            if(total >= cars)
+                return true;
         }
-        return cars<=total;
+        return total >= cars;
     }
 
 public:
     long long repairCars(vector<int>& ranks, int cars) {
         // r *n*n
         // minimum maximum of all possible combination is the answer
+        unordered_map<int, int> freq;
+        for (int r : ranks)
+            freq[r]++;
+            
         long long low = 0;
-        long long high =(long long)cars *(long long)cars * (*max_element(ranks.begin(), ranks.end()));
-        long long ans = 0;
+        long long high = (long long)cars * cars * (*max_element(ranks.begin(), ranks.end()));
+        long long ans = high;
         while (low <= high) {
-            long long mid = (high - low) / 2 + low;
-            if (check(mid, ranks, cars)) {
-                high = mid - 1;
+            long long mid = low + (high - low) / 2;
+            if (check(mid, freq, cars)) {
                 ans = mid;
+                high = mid - 1;
             } else {
                 low = mid + 1;
             }
