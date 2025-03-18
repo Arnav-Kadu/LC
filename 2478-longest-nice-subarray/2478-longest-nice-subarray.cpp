f9d54457344e4a788f2dd215b66451e90eb4ba34
Arnav-Kadu <105575763+Arnav-Kadu@users.noreply.github.com>
2025-03-18 10:04:08 +0530

    Time: 0 ms (100%), Space: 61 MB (64.15%) - LeetHub

--- 2478-longest-nice-subarray/2478-longest-nice-subarray.cpp
+++ 2478-longest-nice-subarray/2478-longest-nice-subarray.cpp
@@ -1,47 +1,60 @@
 class Solution {
-    int traverse(vector<int>& cnt, const string &binary) {
-        int conflict = -1;
-        for (int i = 0; i < 32; i++) {
-            if (binary[i] == '1') {
-                if (cnt[i] > 0) conflict = i;
-                cnt[i]++;
-            }
-        }
-        return conflict;
-    }
-    
-    bool conflictExists(const vector<int>& cnt) {
-        for (int i = 0; i < 32; i++) {
-            if (cnt[i] > 1) return true;
-        }
-        return false;
-    }
-    
+    // int traverse(vector<int>& cnt, const string &binary) {
+    //     int conflict = -1;
+    //     for (int i = 0; i < 32; i++) {
+    //         if (binary[i] == '1') {
+    //             if (cnt[i] > 0) conflict = i;
+    //             cnt[i]++;
+    //         }
+    //     }
+    //     return conflict;
+    // }
+
+    // bool conflictExists(const vector<int>& cnt) {
+    //     for (int i = 0; i < 32; i++) {
+    //         if (cnt[i] > 1) return true;
+    //     }
+    //     return false;
+    // }
+
 public:
     int longestNiceSubarray(vector<int>& nums) {
         int n = nums.size(), ans = 0;
-        vector<int> cnt(33, 0);
-        int start = 0, end = 0;
-        while(end < n) {
-            bitset<32> b(nums[end]);
-            string temp = b.to_string();
-            int check = traverse(cnt, temp);
-            if(check == -1) {
-                ans = max(ans, end - start + 1);
-                end++;
-            } else {
-                while(conflictExists(cnt)) {
-                    bitset<32> b2(nums[start]);
-                    string s2 = b2.to_string();
-                    for (int i = 0; i < 32; i++) {
-                        if(s2[i] == '1')
-                            cnt[i]--;
-                    }
-                    start++;
-                }
-                ans = max(ans, end - start + 1);
-                end++;
+        // vector<int> cnt(33, 0);
+        // int start = 0, end = 0;
+        // while(end < n) {
+        //     bitset<32> b(nums[end]);
+        //     string temp = b.to_string();
+        //     int check = traverse(cnt, temp);
+        //     if(check == -1) {
+        //         ans = max(ans, end - start + 1);
+        //         end++;
+        //     } else {
+        //         while(conflictExists(cnt)) {
+        //             bitset<32> b2(nums[start]);
+        //             string s2 = b2.to_string();
+        //             for (int i = 0; i < 32; i++) {
+        //                 if(s2[i] == '1')
+        //                     cnt[i]--;
+        //             }
+        //             start++;
+        //         }
+        //         ans = max(ans, end - start + 1);
+        //         end++;
+        //     }
+        // }
+        // return ans;
+        int start = 0;
+        int end = 0;
+        int used_bits = 0;
+        while (end < n) {
+            while ((used_bits & nums[end]) != 0) {
+                used_bits ^= nums[start];
+                start++;
             }
+            used_bits |= nums[end];
+            ans = max(ans, end - start + 1);
+            end++;
         }
         return ans;
     }
