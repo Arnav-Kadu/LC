74be13f3d35e4c65af161934d77f10d00913fb3f
Arnav-Kadu <105575763+Arnav-Kadu@users.noreply.github.com>
2025-03-24 12:45:19 +0530

    Time: 69 ms (59.69%), Space: 126.5 MB (59.18%) - LeetHub

---
+++ 3430-count-days-without-meetings/3430-count-days-without-meetings.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int countDays(int days, vector<vector<int>>& meetings) {
+        sort(meetings.begin() , meetings.end());
+
+        int strt = meetings[0][0];
+        int end = meetings[0][1];
+
+        for(int i=1 ; i<meetings.size() ; i++){
+            if(meetings[i][0] <= end){
+                end = max(end,meetings[i][1]);
+            }
+            else{
+                days -= end-strt+1; // this days are occupied
+                strt = meetings[i][0];
+                end = meetings[i][1];
+            }
+        }
+        days -= end-strt+1;
+
+        return days;
+    }
+};
