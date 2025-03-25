3bb68df95d5e6591f2564d4fe0d8dbeac9957181
Arnav-Kadu <105575763+Arnav-Kadu@users.noreply.github.com>
2025-03-25 13:08:58 +0530

    Time: 284 ms (45.08%), Space: 247.4 MB (43.98%) - LeetHub

---
+++ 3657-check-if-grid-can-be-cut-into-sections/3657-check-if-grid-can-be-cut-into-sections.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool checkValidCuts(int n, vector<vector<int>>& r) {
+        vector<vector<int>> rx = r, ry = r;
+        sort(rx.begin(), rx.end(), [](const vector<int>& a, const vector<int>& b) {
+            return a[0] < b[0];
+        });
+        sort(ry.begin(), ry.end(), [](const vector<int>& a, const vector<int>& b) {
+            return a[1] < b[1];
+        });
+        int gx = 0, gy = 0;
+        int ex = rx[0][2], ey = ry[0][3];
+        for (size_t i = 1; i < r.size(); i++) {
+            if (ex <= rx[i][0]) gx++;
+            ex = max(ex, rx[i][2]);
+            if (ey <= ry[i][1]) gy++;
+            ey = max(ey, ry[i][3]);
+        }
+        return gx >= 2 || gy >= 2;
+    }
+};
