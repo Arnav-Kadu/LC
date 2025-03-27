7490573f79e8726bf9fda295ae4ecfebb6299cf3
Arnav-Kadu <105575763+Arnav-Kadu@users.noreply.github.com>
2025-03-27 11:43:51 +0530

    Time: 79 ms (8.2%), Space: 99.4 MB (34.84%) - LeetHub

---
+++ 2888-minimum-index-of-a-valid-split/2888-minimum-index-of-a-valid-split.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int minimumIndex(vector<int>& nums) {
+        int mx = INT_MIN;
+        int which = -1;
+        map<int, int> m;
+        for (auto i : nums) {
+            m[i]++;
+            if (m[i] > mx) {
+                which = i;
+                mx = m[i];
+            }
+        }
+        if (mx <= nums.size() / 2) {
+            return -1;
+        }
+        int curr = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (which == nums[i]) {
+                curr++;
+            }
+            if (curr * 2 > (i + 1) && (m[which] - curr) * 2 > (nums.size() - i - 1)) {
+                return i ;
+            }
+        }
+        return -1;
+    }
+};
