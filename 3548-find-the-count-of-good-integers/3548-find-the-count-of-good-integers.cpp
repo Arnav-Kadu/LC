f0dadaa4230a450931b1cab66760678625cffc44
Arnav-Kadu <105575763+Arnav-Kadu@users.noreply.github.com>
2025-04-12 11:37:42 +0530

    Time: 301 ms (63.79%), Space: 41.4 MB (62.07%) - LeetHub

--- 3548-find-the-count-of-good-integers/3548-find-the-count-of-good-integers.cpp
+++ 3548-find-the-count-of-good-integers/3548-find-the-count-of-good-integers.cpp
@@ -1,39 +1,57 @@
 class Solution {
 public:
-    long long countGoodIntegers(int n, int k) {
-        unordered_set<string> dict;
-        int base = pow(10, (n - 1) / 2);
-        int skip = n & 1;
-        /* Enumerate the number of palindrome numbers of n digits */
-        for (int i = base; i < base * 10; i++) {
-            string s = to_string(i);
-            s += string(s.rbegin() + skip, s.rend());
-            long long palindromicInteger = stoll(s);
-            /* If the current palindrome number is a k-palindromic integer */
-            if (palindromicInteger % k == 0) {
-                sort(s.begin(), s.end());
-                dict.emplace(s);
-            }
+    long long dp[11] = {1, 1, -1, -1, -1, -1, -1, -1, -1, -1, -1};
+    unordered_set<string> vis;
+    long long fact(int n) {
+        if (dp[n] != -1)
+            return dp[n];
+        return dp[n] = n * fact(n - 1);
+    }
+    long long solve(string temp, int ind, int x, int k, int n) {
+        if (ind == x) {
+            long long j = stoll(temp);
+            if (j % k == 0) {
+                sort(temp.begin(), temp.end());
+                if (vis.find(temp) != vis.end())
+                    return 0;
+                vis.insert(temp);
+                unordered_map<char, int> mp;
+                for (auto it : temp) {
+                    mp[it]++;
+                }
+                int m = n - mp['0'];
+                long long ans = 1;
+                for (int i = 0; i < n; i++) {
+                    if (i == 1)
+                        m += mp['0'];
+                    ans = ans * m;
+                    m--;
+                }
+                for (auto [v, c] : mp) {
+                    if (c != 0)
+                        ans /= fact(c);
+                }
+                return ans;
+            } else
+                return 0;
         }
-
-        vector<long long> factorial(n + 1, 1);
         long long ans = 0;
-        for (int i = 1; i <= n; i++) {
-            factorial[i] = factorial[i - 1] * i;
-        }
-        for (const string &s : dict) {
-            vector<int> cnt(10);
-            for (char c : s) {
-                cnt[c - '0']++;
-            }
-            /* Calculate permutations and combinations */
-            long long tot = (n - cnt[0]) * factorial[n - 1];
-            for (int x : cnt) {
-                tot /= factorial[x];
-            }
-            ans += tot;
-        }
+        for (int i = 0; i <= 9; i++) {
+            if (ind == 0 && i == 0)
+                continue;
 
+            char orr = temp[ind];
+            temp[ind] = (i + '0');
+            temp[n - ind - 1] = (i + '0');
+            ans += solve(temp, ind + 1, x, k, n);
+            temp[ind] = orr;
+            temp[n - ind - 1] = orr;
+        }
         return ans;
+    }
+    long long countGoodIntegers(int n, int k) {
+        string temp(n, '0');
+        int x = (n + 1) / 2;
+        return solve(temp, 0, x, k, n);
     }
 };
