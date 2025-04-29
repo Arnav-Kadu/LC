bd9962aff15735e72b89cdae270d414d02f753a8
Arnav-Kadu <105575763+Arnav-Kadu@users.noreply.github.com>
2025-04-29 08:39:55 +0530

    Time: 235 ms (5.05%), Space: 153.5 MB (5.36%) - LeetHub

--- 3213-count-subarrays-where-max-element-appears-at-least-k-times/3213-count-subarrays-where-max-element-appears-at-least-k-times.cpp
+++ 3213-count-subarrays-where-max-element-appears-at-least-k-times/3213-count-subarrays-where-max-element-appears-at-least-k-times.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
-    long long countSubarrays(vector<int>& nums, int k) 
-    {
-        int mx = *max_element(nums.begin(), nums.end());
-        int n=nums.size();
-        int cnt=0;
-        int left=0;
-        long long ans=0;
-        for(int i=0;i<n;i++){
-            if(mx==nums[i]){
-                cnt++;
+    long long countSubarrays(vector<int>& nums, int k) {
+        int n = nums.size();
+        long long ans = 0;
+        int j = 0;
+        map<int, int> m;
+        int mx=*max_element(nums.begin(),nums.end());
+        for (int i = 0; i < n; i++) {
+            m[nums[i]]++;
+            while (m[mx] >= k) {
+                ans += n - i;
+                m[nums[j]]--;
+                j++;
             }
-            while(cnt==k){
-                cnt-=(nums[left]==mx);
-                left++;
-            }
-            ans+=(i-left+1);
         }
-        return (n*1LL*(n+1)/2)-ans;
+        
+        return ans;
     }
 };
