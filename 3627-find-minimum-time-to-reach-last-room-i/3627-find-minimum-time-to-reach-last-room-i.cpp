d4687cb67288f5345c001851471b237496ebba72
Arnav-Kadu <105575763+Arnav-Kadu@users.noreply.github.com>
2025-05-07 19:14:23 +0530

    Time: 16 ms (86.17%), Space: 29.3 MB (63.33%) - LeetHub

--- 3627-find-minimum-time-to-reach-last-room-i/3627-find-minimum-time-to-reach-last-room-i.cpp
+++ 3627-find-minimum-time-to-reach-last-room-i/3627-find-minimum-time-to-reach-last-room-i.cpp
@@ -6,27 +6,34 @@ public:
     int minTimeToReach(vector<vector<int>>& moveTime) {
         int n = moveTime.size();
         int m = moveTime[0].size();
-        priority_queue<
-            pair<int, pair<int,int>>,
-            vector<pair<int, pair<int,int>>>,
-            greater<pair<int, pair<int,int>>>
-        > pq;
+        // what what i need {current time, {x,y}}
+        priority_queue<pair<int, pair<int, int>>,
+                       vector<pair<int, pair<int, int>>>,
+                       greater<pair<int, pair<int, int>>>>
+            pq;
         vector<vector<int>> vis(n, vector<int>(m, 0));
         pq.push({0, {0, 0}});
         while (!pq.empty()) {
-            auto top = pq.top(); pq.pop();
+            auto top = pq.top();
+            pq.pop();
             int cost = top.first;
             int x = top.second.first;
             int y = top.second.second;
-            if (vis[x][y]) continue;
+            if(vis[x][y]) continue;
             vis[x][y] = 1;
-            if (x == n - 1 && y == m - 1) return cost;
+            if (x == n - 1 && y == m - 1) {
+                return cost;
+            }
             for (int i = 0; i < 4; i++) {
                 int nx = x + delrow[i];
                 int ny = y + delcol[i];
-                if (nx < 0 || ny < 0 || nx >= n || ny >= m || vis[nx][ny]) continue;
-                int enter = max(cost, moveTime[nx][ny]) + 1;
-                pq.push({enter, {nx, ny}});
+                if (nx >= 0 && ny >= 0 && nx < n && ny < m && !vis[nx][ny]) {
+                    if (moveTime[nx][ny] <= cost) {
+                        pq.push({cost + 1, {nx, ny}});
+                    } else {
+                        pq.push({moveTime[nx][ny] + 1, {nx, ny}});
+                    }
+                }
             }
         }
         return -1;
