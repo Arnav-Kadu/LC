45eea1f0380ff308e2398da4cc81e68157723d93
Arnav-Kadu <105575763+Arnav-Kadu@users.noreply.github.com>
2025-05-07 19:11:45 +0530

    Time: 19 ms (76.17%), Space: 29.4 MB (63.09%) - LeetHub

---
+++ 3627-find-minimum-time-to-reach-last-room-i/3627-find-minimum-time-to-reach-last-room-i.cpp
@@ -0,0 +1,34 @@
+class Solution {
+    vector<int> delrow = {0, -1, 0, 1};
+    vector<int> delcol = {-1, 0, 1, 0};
+
+public:
+    int minTimeToReach(vector<vector<int>>& moveTime) {
+        int n = moveTime.size();
+        int m = moveTime[0].size();
+        priority_queue<
+            pair<int, pair<int,int>>,
+            vector<pair<int, pair<int,int>>>,
+            greater<pair<int, pair<int,int>>>
+        > pq;
+        vector<vector<int>> vis(n, vector<int>(m, 0));
+        pq.push({0, {0, 0}});
+        while (!pq.empty()) {
+            auto top = pq.top(); pq.pop();
+            int cost = top.first;
+            int x = top.second.first;
+            int y = top.second.second;
+            if (vis[x][y]) continue;
+            vis[x][y] = 1;
+            if (x == n - 1 && y == m - 1) return cost;
+            for (int i = 0; i < 4; i++) {
+                int nx = x + delrow[i];
+                int ny = y + delcol[i];
+                if (nx < 0 || ny < 0 || nx >= n || ny >= m || vis[nx][ny]) continue;
+                int enter = max(cost, moveTime[nx][ny]) + 1;
+                pq.push({enter, {nx, ny}});
+            }
+        }
+        return -1;
+    }
+};
