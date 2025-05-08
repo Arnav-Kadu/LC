deda7f7d14a00f68bff2c679b63e94eca00e5b44
Arnav-Kadu <105575763+Arnav-Kadu@users.noreply.github.com>
2025-05-08 12:14:19 +0530

    Time: 347 ms (65.19%), Space: 106.1 MB (56.12%) - LeetHub

---
+++ 3628-find-minimum-time-to-reach-last-room-ii/3628-find-minimum-time-to-reach-last-room-ii.cpp
@@ -0,0 +1,45 @@
+class Solution {
+    vector<int> delrow = {0, -1, 0, 1};
+    vector<int> delcol = {-1, 0, 1, 0};
+
+public:
+    int minTimeToReach(vector<vector<int>>& moveTime) {
+        int n = moveTime.size();
+        int m = moveTime[0].size();
+        // what what i need {current time, {flipper,{x,y}}}
+        priority_queue<pair<int, pair<int, pair<int, int>>>,
+                       vector<pair<int, pair<int, pair<int, int>>>>,
+                       greater<pair<int, pair<int, pair<int, int>>>>>
+            pq;
+        vector<vector<int>> vis(n, vector<int>(m, 0));
+        pq.push({0, {0, {0, 0}}});
+        while (!pq.empty()) {
+            auto top = pq.top();
+            pq.pop();
+            int cost = top.first;
+            int x = top.second.second.first;
+            int y = top.second.second.second;
+            int flipper = top.second.first;
+            if (vis[x][y])
+                continue;
+            vis[x][y] = 1;
+            if (x == n - 1 && y == m - 1) {
+                return cost;
+            }
+            for (int i = 0; i < 4; i++) {
+                int nx = x + delrow[i];
+                int ny = y + delcol[i];
+                if (nx >= 0 && ny >= 0 && nx < n && ny < m && !vis[nx][ny]) {
+                    if (moveTime[nx][ny] <= cost) {
+                        pq.push(
+                            {cost + 1 + 1 * flipper, {flipper ^ 1, {nx, ny}}});
+                    } else {
+                        pq.push({moveTime[nx][ny] + 1 + 1 * flipper,
+                                 {flipper ^ 1, {nx, ny}}});
+                    }
+                }
+            }
+        }
+        return -1;
+    }
+};
