b325ccf34e464dfae1b631d442295c2e9b050f9b
Arnav-Kadu <105575763+Arnav-Kadu@users.noreply.github.com>
2025-05-13 20:54:52 +0530

    Time: 65 ms (87.19%), Space: 19.1 MB (84.24%) - LeetHub

--- 3629-total-characters-in-string-after-transformations-i/3629-total-characters-in-string-after-transformations-i.cpp
+++ 3629-total-characters-in-string-after-transformations-i/3629-total-characters-in-string-after-transformations-i.cpp
@@ -1,26 +1,24 @@
 class Solution {
 public:
     int lengthAfterTransformations(string s, int t) {
-        vector<int> cnt(26);
-        for (char ch : s) {
-            ++cnt[ch - 'a'];
+        vector<long long> chars(27,0);
+        int n=s.size();
+        for(int i=0; i<n; i++){
+            chars[s[i]-'a']++;
         }
-        for (int round = 0; round < t; ++round) {
-            vector<int> nxt(26);
-            nxt[0] = cnt[25];
-            nxt[1] = (cnt[25] + cnt[0]) % mod;
-            for (int i = 2; i < 26; ++i) {
-                nxt[i] = cnt[i - 1];
+        while(t>0){
+            for(int i=26; i>0; i--){
+                chars[i]=chars[i-1];
             }
-            cnt = move(nxt);
+            chars[0] = chars[26];
+            chars[1] = (chars[1]+chars[26])%1000000007;
+            chars[26] = 0;
+            t--;
         }
         int ans = 0;
-        for (int i = 0; i < 26; ++i) {
-            ans = (ans + cnt[i]) % mod;
+        for(int i=0; i<26; i++){
+            ans = (ans + chars[i])%1000000007;
         }
         return ans;
     }
-
-private:
-    static constexpr int mod = 1000000007;
 };
