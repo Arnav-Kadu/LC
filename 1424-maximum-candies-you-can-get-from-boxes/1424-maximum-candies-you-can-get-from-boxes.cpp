166c34e2fefae39a54452f43f81f516c069e176e
Arnav-Kadu <105575763+Arnav-Kadu@users.noreply.github.com>
2025-06-03 07:52:17 +0530

    Time: 0 ms (100%), Space: 44.2 MB (66.83%) - LeetHub

--- 1424-maximum-candies-you-can-get-from-boxes/1424-maximum-candies-you-can-get-from-boxes.cpp
+++ 1424-maximum-candies-you-can-get-from-boxes/1424-maximum-candies-you-can-get-from-boxes.cpp
@@ -1,48 +1,36 @@
 class Solution {
 public:
-    int maxCandies(vector<int>& status, vector<int>& candies, vector<vector<int>>& keys, vector<vector<int>>& containedBoxes, vector<int>& initialBoxes) {
-        // status
-        // candies
-        // keys
-        // containedBoxes
-        // initial array
-        // kahn's algo?
-
-        int n = status.size();
+    int maxCandies(vector<int>& status, vector<int>& candies,vector<vector<int>>& keys,vector<vector<int>>& containedBoxes,vector<int>& initialBoxes) {
+        int cnt = 0;
         queue<int> q;
-        set<int> dowehavekey;
-        vector<bool> haveBox(n, false);
-        vector<bool> vis(n, false);
-        for (int b : initialBoxes) {
-            haveBox[b] = true;
-            if (status[b] == 1) {
-                q.push(b);
-                vis[b] = true;
-            }
+        for (auto it : initialBoxes) {
+            q.push(it);
         }
-        int ans = 0;
         while (!q.empty()) {
-            int top = q.front();
-            q.pop();
-            ans += candies[top];
-            for (int k : keys[top]) {
-                if (dowehavekey.insert(k).second) {
-                    if (haveBox[k] && !vis[k]) {
-                        q.push(k);
-                        vis[k] = true;
+            int sz = q.size();
+            vector<int> curr;
+            vector<int> inserted;
+            while (sz--) {
+                int box = q.front();
+                q.pop();
+                curr.push_back(box);
+                if (status[box]) {
+                    cnt += candies[box];
+                    for (auto key : keys[box]) {
+                        status[key] = 1;
                     }
+                    for (auto other : containedBoxes[box]) {
+                        q.push(other);
+                    }
+                } else {
+                    q.push(box);
+                    inserted.push_back(box);
                 }
             }
-            for (int nb : containedBoxes[top]) {
-                if (!haveBox[nb]) {
-                    haveBox[nb] = true;
-                    if ((status[nb] == 1 || dowehavekey.count(nb)) && !vis[nb]) {
-                        q.push(nb);
-                        vis[nb] = true;
-                    }
-                }
+            if (curr == inserted) {
+                break;
             }
         }
-        return ans;
+        return cnt;
     }
 };
