9ff1c3b79cefde6764f130528bd483cfca67e345
Arnav-Kadu <105575763+Arnav-Kadu@users.noreply.github.com>
2025-06-23 22:45:37 +0530

    Time: 83 ms (79.82%), Space: 7.9 MB (93.86%) - LeetHub

--- 2202-sum-of-k-mirror-numbers/2202-sum-of-k-mirror-numbers.cpp
+++ 2202-sum-of-k-mirror-numbers/2202-sum-of-k-mirror-numbers.cpp
@@ -1,47 +1,55 @@
 class Solution {
-private:
-    int digit[100];
-
 public:
-    long long kMirror(int k, int n) {
-        auto isPalindrome = [&](long long x) -> bool {
-            int length = -1;
-            while (x) {
-                ++length;
-                digit[length] = x % k;
-                x /= k;
-            }
-            for (int i = 0, j = length; i < j; ++i, --j) {
-                if (digit[i] != digit[j]) {
-                    return false;
-                }
-            }
-            return true;
-        };
+    bool base10ToKPalindrome(long long n, int base) {
+        long long original = n;
+        long long rev = 0;
 
-        int left = 1, count = 0;
-        long long ans = 0;
-        while (count < n) {
-            int right = left * 10;
-            // op = 0 indicates enumerating odd-length palindromes
-            // op = 1 indicates enumerating even-length palindromes
-            for (int op = 0; op < 2; ++op) {
-                // enumerate i'
-                for (int i = left; i < right && count < n; ++i) {
-                    long long combined = i;
-                    int x = (op == 0 ? i / 10 : i);
-                    while (x) {
-                        combined = combined * 10 + x % 10;
-                        x /= 10;
-                    }
-                    if (isPalindrome(combined)) {
-                        ++count;
-                        ans += combined;
-                    }
-                }
-            }
-            left = right;
+        while (n > 0) {
+            rev = rev * base + (n % base);
+            n /= base;
         }
-        return ans;
+
+        return rev == original;
+    }
+
+    long long create_palindrome(long long half, bool odd) {
+        long long res = half;
+        if (odd) res /= 10;
+
+        while (res > 0) {
+            half = half * 10 + (res % 10);
+            res /= 10;
+        }
+
+        return half;
+    }
+
+    long long kMirror(int k, int n) {
+        int count = 0;
+        long long odd_num = 1, even_num = 1;
+        long long sum = 0;
+
+        long long odd_palindrome = create_palindrome(odd_num, true);
+        long long even_palindrome = create_palindrome(even_num, false);
+
+        while (count < n) {
+            if (odd_palindrome <= even_palindrome) {
+                if (base10ToKPalindrome(odd_palindrome, k)) {
+                    sum += odd_palindrome;
+                    count++;
+                }
+                odd_num++;
+                odd_palindrome = create_palindrome(odd_num, true);
+            } else {
+                if (base10ToKPalindrome(even_palindrome, k)) {
+                    sum += even_palindrome;
+                    count++;
+                }
+                even_num++;
+                even_palindrome = create_palindrome(even_num, false);
+            }
+        }
+
+        return sum;
     }
 };
