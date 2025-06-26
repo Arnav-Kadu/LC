4a7982c26712be549dfa0eb263d6eac654205fe0
Arnav-Kadu <105575763+Arnav-Kadu@users.noreply.github.com>
2025-06-26 21:27:57 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2395-longest-binary-subsequence-less-than-or-equal-to-k/2395-longest-binary-subsequence-less-than-or-equal-to-k.cpp
+++ 2395-longest-binary-subsequence-less-than-or-equal-to-k/2395-longest-binary-subsequence-less-than-or-equal-to-k.cpp
@@ -9,7 +9,6 @@ private:
                 if (output > K) return K + 1;  
             }
             res <<= 1;
-            if (res > K) res = (long long)K + 1;
         }
         return (int)output;
     }
