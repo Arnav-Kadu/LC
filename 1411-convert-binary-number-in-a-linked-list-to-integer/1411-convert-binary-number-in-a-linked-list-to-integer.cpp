57db913b380ef3e7213d92d2dc4159e27d26e639
Arnav-Kadu <105575763+Arnav-Kadu@users.noreply.github.com>
2025-07-14 11:07:25 +0530

    Time: 0 ms (100%), Space: 10.6 MB (71.97%) - LeetHub

--- 1411-convert-binary-number-in-a-linked-list-to-integer/1411-convert-binary-number-in-a-linked-list-to-integer.cpp
+++ 1411-convert-binary-number-in-a-linked-list-to-integer/1411-convert-binary-number-in-a-linked-list-to-integer.cpp
@@ -11,18 +11,10 @@
 class Solution {
 public:
     int getDecimalValue(ListNode* head) {
-        string s;
-        while(head!=NULL){
-            s+=to_string(head->val);
-            head=head->next;
-        }
-        reverse(s.begin(),s.end());
-        cout<<s<<endl;
         int ans=0;
-        int multi=1;
-        for(auto i:s){
-            ans+=(i-'0')*multi;
-            multi*=2;
+        while(head!=NULL){
+            ans=(ans<<1)+head->val;
+            head=head->next;
         }
         return ans;
     }
