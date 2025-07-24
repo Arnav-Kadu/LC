d117ef173a7dd76008dde650d8ca06fc210cdd25
Arnav-Kadu <105575763+Arnav-Kadu@users.noreply.github.com>
2025-07-24 10:34:15 +0530

    Time: 83 ms (86.51%), Space: 18 MB (96.03%) - LeetHub

--- 2400-minimum-score-after-removals-on-a-tree/2400-minimum-score-after-removals-on-a-tree.cpp
+++ 2400-minimum-score-after-removals-on-a-tree/2400-minimum-score-after-removals-on-a-tree.cpp
@@ -1,57 +1,31 @@
 class Solution {
-public:
-    int calc(int part1, int part2, int part3) {
-        return max(part1, max(part2, part3)) - min(part1, min(part2, part3));
+    int dp[1000] = {}, last[1000] = {};
+    int dfs(vector<int>& nums,vector<vector<int>>& adj,int node,int par,int &ids) {
+        int res = nums[node],id = ids++;
+        for(auto i : adj[node]) {
+            if(i != par) {
+                res ^= dfs(nums,adj,i,node,ids);
+            }
+        }
+        last[id] = ids;
+        return dp[id] = res;
     }
-
-    int minimumScore(vector<int> &nums, vector<vector<int>> &edges) {
-        int n = nums.size();
-        vector<vector<int>> e(n);
-        for (auto &v : edges) {
-            e[v[0]].push_back(v[1]);
-            e[v[1]].push_back(v[0]);
+public:
+    int minimumScore(vector<int>& nums, vector<vector<int>>& edges) {
+        int ids = 0, ans = INT_MAX, n = nums.size();
+        vector<vector<int>> adj(n);
+        for(auto &e : edges) {
+            adj[e[0]].push_back(e[1]);
+            adj[e[1]].push_back(e[0]);
         }
-
-        int sum = 0;
-        for (int x : nums) {
-            sum ^= x;
+        int tot = dfs(nums,adj,0,-1,ids);
+        for(int i=1;i<n;i++) {
+            for(int j=i+1;j<n;j++) {
+                int op1 = j<last[i] ? tot^dp[i] : tot^dp[i]^dp[j];
+                int op2 = j<last[i] ? dp[i]^dp[j] : dp[i];
+                ans = min(ans,max({op1,op2,dp[j]}) - min({op1,op2,dp[j]}));
+            }
         }
-
-        int res = INT_MAX;
-        function<int(int, int, int, int)> dfs2 = [&](int x, int f, int oth,
-                                                     int anc) {
-            int son = nums[x];
-            for (auto &y : e[x]) {
-                if (y == f) {
-                    continue;
-                }
-                son ^= dfs2(y, x, oth, anc);
-            }
-            if (f == anc) {
-                return son;
-            }
-            res = min(res, calc(oth, son, sum ^ oth ^ son));
-            return son;
-        };
-
-        function<int(int, int)> dfs = [&](int x, int f) {
-            int son = nums[x];
-            for (auto &y : e[x]) {
-                if (y == f) {
-                    continue;
-                }
-                son ^= dfs(y, x);
-            }
-
-            for (auto &y : e[x]) {
-                if (y == f) {
-                    dfs2(y, x, son, x);
-                }
-            }
-            return son;
-        };
-
-        dfs(0, -1);
-        return res;
+        return ans;
     }
 };
