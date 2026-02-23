59201eb4fa820cd78e840aab6e64a590393fb027
Arnav-Kadu <105575763+Arnav-Kadu@users.noreply.github.com>
2026-02-23 10:17:00 +0530

    Time: 541 ms (29.16%), Space: 102.3 MB (68.3%) - LeetHub

--- 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.cpp
+++ 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.cpp
@@ -1,20 +1,16 @@
 class Solution {
+
 public:
     bool hasAllCodes(string s, int k) {
-        int n = s.size();
-        if (k > n) return false;
-        int need = 1 << k;
+        if (s.empty() || s.size() < k)
+            return false;
+
         set<int> st;
-        int mask = need - 1;
-        int val = 0;
-        for (int i = 0; i < k; ++i) val = (val << 1) | (s[i] - '0');
-        st.insert(val);
-        if ((int)st.size() == need) return true;
-        for (int i = k; i < n; ++i) {
-            val = ((val << 1) & mask) | (s[i] - '0');
-            st.insert(val);
-            if ((int)st.size() == need) return true;
+        for (auto i = 0; i <= s.length() - k; i++) {
+            string holder = s.substr(i, k);
+            st.insert(atoi(&holder[0]));
         }
-        return (int)st.size() == need;
+
+        return (st.size() == pow(2, k)) ? true : false;
     }
 };
