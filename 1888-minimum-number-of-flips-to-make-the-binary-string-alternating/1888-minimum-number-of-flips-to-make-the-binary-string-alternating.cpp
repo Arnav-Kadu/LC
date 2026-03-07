a645c80f86be1340dcdf5b26bcde7401129b7032
Arnav-Kadu <105575763+Arnav-Kadu@users.noreply.github.com>
2026-03-07 20:19:42 +0530

    Time: 11 ms (81.6%), Space: 17.4 MB (54%) - LeetHub

---
+++ 1888-minimum-number-of-flips-to-make-the-binary-string-alternating/1888-minimum-number-of-flips-to-make-the-binary-string-alternating.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    int minFlips(string s) {
+        // we need to find min cost in segment of size n in string of length 2*n that is s+=s;
+        int n=s.size();
+        s+=s;
+        int type1=0;
+        int type2=1;
+        int count1=0;
+        int count2=0;
+        int mn1=INT_MAX;
+        int mn2=INT_MAX;
+        for(int i=0;i<n;i++){
+           if(type1!=s[i]-'0'){
+              count1++;
+           }
+           if(type2!=s[i]-'0'){
+            count2++;
+           }
+           type1^=1;
+           type2^=1;
+        }
+        mn1=count1;
+        mn2=count2;
+        int type11=0;
+        int type22=1;
+        for(int i=n;i<2*n;i++){
+           if(type11!=s[i-n]-'0'){
+             count1--;
+           }
+           if(type22!=s[i-n]-'0'){
+            count2--;
+           }
+           // removed from previous check current
+           if(type1!=s[i]-'0'){
+              count1++;
+           }
+           if(type2!=s[i]-'0'){
+            count2++;
+           }
+           mn1=min(mn1,count1);
+           mn2=min(mn2,count2);
+           type1^=1;
+           type2^=1;
+           type11^=1;
+           type22^=1;
+        }
+        // cout<<mn1<<" "<<mn2<<endl;
+        return min(mn1,mn2);
+    }
+};
