af5c682ad3f78948ed0cba2a1efe41f8e48141bb
Arnav-Kadu <105575763+Arnav-Kadu@users.noreply.github.com>
2026-03-19 16:16:14 +0530

    Time: 17 ms (96.75%), Space: 101.2 MB (89.43%) - LeetHub

--- 3212-count-submatrices-with-equal-frequency-of-x-and-y/3212-count-submatrices-with-equal-frequency-of-x-and-y.cpp
+++ 3212-count-submatrices-with-equal-frequency-of-x-and-y/3212-count-submatrices-with-equal-frequency-of-x-and-y.cpp
@@ -1,27 +1,22 @@
 class Solution {
 public:
     int numberOfSubmatrices(vector<vector<char>>& grid) {
-        int m = grid.size(), n = grid[0].size();
-        int ans = 0;
-
-        vector<int> px(n, 0), py(n, 0);
-
-        for (int i = 0; i < m; i++) {
-            int rowX = 0, rowY = 0;
-
-            for (int j = 0; j < n; j++) {
-                if (grid[i][j] == 'X') rowX++;
-                else if (grid[i][j] == 'Y') rowY++;
-
-                px[j] += rowX;
-                py[j] += rowY;
-
-                if (px[j] == py[j] && px[j] > 0) {
-                    ans++;
-                }
+        const int rows = grid.size();
+        const int cols = grid[0].size();
+        vector<int> xCount(cols);
+        vector<int> yCount(cols);
+        auto count = 0;
+        for (auto row = 0; row < rows; ++row) {
+            auto rowX = 0;
+            auto rowY = 0;
+            for (auto col = 0; col < cols; ++col) {
+                rowX += grid[row][col] == 'X';
+                rowY += grid[row][col] == 'Y';
+                xCount[col] += rowX;
+                yCount[col] += rowY;
+                if (xCount[col] && xCount[col] == yCount[col]) ++count;
             }
         }
-
-        return ans;
+        return count;
     }
 };
