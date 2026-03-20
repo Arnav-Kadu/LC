ee3566aea0238ec0971daa056d8c42a4d83c2f8f
Arnav-Kadu <105575763+Arnav-Kadu@users.noreply.github.com>
2026-03-20 17:21:37 +0530

    Time: 12 ms (80%), Space: 31.9 MB (81.48%) - LeetHub

--- 3567-minimum-absolute-difference-in-sliding-submatrix/3567-minimum-absolute-difference-in-sliding-submatrix.cpp
+++ 3567-minimum-absolute-difference-in-sliding-submatrix/3567-minimum-absolute-difference-in-sliding-submatrix.cpp
@@ -1,29 +1,25 @@
 class Solution {
 public:
-    vector<vector<int>> minAbsDiff(vector<vector<int>>& grid, int k) {
-        int m = grid.size(), n = grid[0].size();
-        vector<vector<int>> res(m - k + 1, vector<int>(n - k + 1));
-        for (int i = 0; i + k <= m; i++) {
-            for (int j = 0; j + k <= n; j++) {
-                vector<int> kgrid;
-                for (int x = i; x < i + k; x++) {
-                    for (int y = j; y < j + k; y++) {
-                        kgrid.push_back(grid[x][y]);
+    vector<vector<int>> minAbsDiff(vector<vector<int>>& grid, int v) {
+        int m = grid.size();
+        int n = grid[0].size();
+        vector<vector<int>>ans(m-v+1, vector<int>(n-v+1, 0));
+        for(int i = 0; i<=m-v; i++){
+            for(int j = 0; j<=n-v; j++){
+                vector<int>temp;
+                for(int k = i; k<i+v; k++){
+                    for(int l = j; l<j+v; l++){
+                        temp.push_back(grid[k][l]);
                     }
                 }
-                int kmin = INT_MAX;
-                sort(kgrid.begin(), kgrid.end());
-                for (int t = 1; t < kgrid.size(); t++) {
-                    if (kgrid[t] == kgrid[t - 1]) {
-                        continue;
-                    }
-                    kmin = min(kmin, kgrid[t] - kgrid[t - 1]);
+                int diff = INT_MAX;
+                sort(temp.begin(), temp.end());
+                for(int m = 1; m<temp.size(); m++){
+                    if(temp[m] != temp[m-1]) diff = min(diff, temp[m]-temp[m-1]);
                 }
-                if (kmin != INT_MAX) {
-                    res[i][j] = kmin;
-                }
+                ans[i][j] = (diff==INT_MAX)?0:diff;
             }
         }
-        return res;
+        return ans;
     }
 };
