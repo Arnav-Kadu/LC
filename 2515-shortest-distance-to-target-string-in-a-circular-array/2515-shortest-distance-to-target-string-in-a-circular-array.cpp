ebfcbc15382d23065cc6fc1938b900266e7b4ff7
Arnav-Kadu <105575763+Arnav-Kadu@users.noreply.github.com>
2026-04-15 11:38:39 +0530

    Time: 0 ms (100%), Space: 18.3 MB (37.22%) - LeetHub

--- 2515-shortest-distance-to-target-string-in-a-circular-array/2515-shortest-distance-to-target-string-in-a-circular-array.cpp
+++ 2515-shortest-distance-to-target-string-in-a-circular-array/2515-shortest-distance-to-target-string-in-a-circular-array.cpp
@@ -1,16 +1,13 @@
 class Solution {
 public:
     int closestTarget(vector<string>& words, string target, int startIndex) {
-        int ans = words.size();
-        int n = words.size();
-
-        for (int i = 0; i < n; ++i) {
-            if (words[i] == target) {
-                int dist = abs(i - startIndex);
-                ans = min(ans, min(dist, n - dist));
+        int n=words.size(),ans=INT_MAX;
+        for(int i=0;i<n;i++){
+            if(words[i]==target){
+                int dif=abs(i-startIndex);
+                ans=min(ans,min(dif,n-dif));
             }
         }
-
-        return ans < n ? ans : -1;
+        return ans==INT_MAX?-1:ans;
     }
 };
