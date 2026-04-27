8b1a8814db4bc2964fa3e9276b943c26c628c290
Arnav-Kadu <105575763+Arnav-Kadu@users.noreply.github.com>
2026-04-27 20:13:24 +0530

    Time: 0 ms (100%), Space: 13.4 MB (43.3%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.cpp
+++ 0684-redundant-connection/0684-redundant-connection.cpp
@@ -1,56 +1,48 @@
-#include <bits/stdc++.h>
-using namespace std;
-
 class Solution {
-    vector<int> adj[1001];
-    vector<int> parent;  
-    int cycleStart = -1, cycleEnd = -1;
-
-    bool dfs(int node, int par, vector<int>& vis) {
-        vis[node] = 1;
-        parent[node] = par;
-
-        for (int next : adj[node]) {
-            if (next == par) continue;
-            if (!vis[next]) {
-                if (dfs(next, node, vis)) return true;
-            } else {
-                cycleStart = next;
-                cycleEnd = node;
-                return true;
-            }
+public:
+    vector<int>size, parent;
+    void disjointset(int n){
+        size.resize(n);
+        parent.resize(n);
+        for(int i=0 ; i<n ; i++){
+            size[i] = 1;
+            parent[i] = i;
         }
-        return false;
+    }
+    int findupar(int node){
+        if(node == parent[node])return node;
+        return parent[node] = findupar(parent[node]);
+    }
+    void unionbys(int u, int v){
+        int ulpu = findupar(u);
+        int ulpv = findupar(v);
+        if(ulpu == ulpv) return; 
+        if(size[ulpu] < size[ulpv]){
+            parent[ulpu] = ulpv;
+            size[ulpv] += size[ulpu];
+        }
+        else{
+            parent[ulpv] = ulpu;
+            size[ulpu] += size[ulpv];
+        }
     }
 
-public:
     vector<int> findRedundantConnection(vector<vector<int>>& edges) {
-        int n = 0;
-        for (auto& edge : edges) {
-            adj[edge[0]].push_back(edge[1]);
-            adj[edge[1]].push_back(edge[0]);
-            n = max({n, edge[0], edge[1]});
-        }
+        vector<int>ans(2,0);
+        int n = edges.size();
+        disjointset(n+1);
 
-        vector<int> vis(n + 1, 0);
-        parent.assign(n + 1, -1);
-
-        for (int i = 1; i <= n; i++) {
-            if (!vis[i] && dfs(i, -1, vis)) break;
-        }
-
-        unordered_set<int> cycleNodes;
-        for (int v = cycleEnd; v != -1; v = parent[v]) {
-            cycleNodes.insert(v);
-            if (v == cycleStart) break;
-        }
-
-        for (auto it = edges.rbegin(); it != edges.rend(); ++it) {
-            if (cycleNodes.count(it->at(0)) && cycleNodes.count(it->at(1))) {
-                return *it;
+        for(auto c : edges){
+            int u = c[0];
+            int v = c[1];
+            if(findupar(u) == findupar(v)){
+                ans[0] = u;
+                ans[1] = v;
+            }
+            else{
+                unionbys(u,v);
             }
         }
-
-        return {};
+        return ans;
     }
 };
