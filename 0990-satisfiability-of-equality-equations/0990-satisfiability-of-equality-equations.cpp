dfb44dee67089c13fb274c2939e3f1dabffb5903
Arnav-Kadu <105575763+Arnav-Kadu@users.noreply.github.com>
2026-04-28 10:17:01 +0530

    Time: 0 ms (100%), Space: 15.1 MB (25.54%) - LeetHub

--- 0990-satisfiability-of-equality-equations/0990-satisfiability-of-equality-equations.cpp
+++ 0990-satisfiability-of-equality-equations/0990-satisfiability-of-equality-equations.cpp
@@ -1,17 +1,51 @@
-class Solution {
-    int uf[26];
-    int find(int x) {
-        return uf[x] == x ? x : (uf[x] = find(uf[x]));
+class DSU {
+public:
+    DSU() = default;
+
+    int find(int a) {
+        auto it = parent.find(a);
+
+        if (it == parent.end() || it->second == a) {
+            parent[a] = a;
+            return a;
+        }
+
+        return parent[a] = find(it->second);
     }
+
+    void unite(int u, int v) {
+        int root_u = find(u);
+        int root_j = find(v);
+
+        if (root_u != root_j) {
+            parent[root_u] = root_j;
+        }
+    }
+
+    bool isConnected(int u, int v) { return find(u) == find(v); }
+
+private:
+    std::unordered_map<char, int> parent{};
+};
+
+class Solution {
 public:
     bool equationsPossible(vector<string>& equations) {
-        for (int i = 0; i < 26; ++i) uf[i] = i;
-        for (auto e : equations) {
-            if (e[1] == '=') uf[find(e[0] - 'a')] = find(e[3] - 'a'); 
+        DSU dsu{};
+
+        for (auto& eq : equations) {
+            if (eq[1] == '=') {
+                dsu.unite(eq[0], eq[3]);
+            }
         }
-        for (auto e : equations) {
-            if (e[1] == '!' && find(e[0] - 'a') == find(e[3] - 'a')) return false;
+
+        for (auto &eq : equations) {
+            if(eq[1]=='=') continue;
+            if (dsu.isConnected(eq[0], eq[3])) {
+                return false;
+            }
         }
+
         return true;
     }
 };
