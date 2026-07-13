ab77d430eb84877576da7abb4cf15177444ec2d5
Arnav-Kadu <105575763+Arnav-Kadu@users.noreply.github.com>
2026-07-13 18:11:15 +0530

    Time: 0 ms (100%), Space: 8.4 MB (48.96%) - LeetHub

--- 1291-sequential-digits/1291-sequential-digits.cpp
+++ 1291-sequential-digits/1291-sequential-digits.cpp
@@ -1,19 +1,35 @@
 class Solution {
+    string H;
+    int n;
+    vector<int> out;
+    void go(int pos, int prev, bool tight, bool started, long long cur) {
+        if (pos == n) {
+            if (started)
+                out.push_back((int)cur);
+            return;
+        }
+        int cap = tight ? H[pos] - '0' : 9;
+        for (int d = 0; d <= cap; ++d) {
+            bool nt = tight && (d == cap);
+            if (!started && d == 0)
+                go(pos + 1, -1, nt, false, 0);
+            else if (started && d != prev + 1)
+                continue;
+            else
+                go(pos + 1, d, nt, true, cur * 10 + d);
+        }
+    }
+
 public:
     vector<int> sequentialDigits(int low, int high) {
-       string s="123456789";
-       vector<int>ans;
-       for(int i=0;i<s.size();i++){
-        string curr="";
-        for(int j=i;j<s.size();j++){ 
-            curr+=s[j];
-            int temp=stoi(curr);
-            if(low<=temp && temp<=high){
-                ans.push_back(temp);
-            }
-        }
-       }
-       sort(ans.begin(),ans.end());
-       return ans;
+        H = to_string(high);
+        n = H.size();
+        go(0, -1, true, false, 0);
+        vector<int> ans;
+        for (int v : out)
+            if (v >= low)
+                ans.push_back(v);
+        sort(ans.begin(), ans.end());
+        return ans;
     }
 };
