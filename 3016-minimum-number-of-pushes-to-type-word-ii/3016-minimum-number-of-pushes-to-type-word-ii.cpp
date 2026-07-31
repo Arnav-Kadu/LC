97c572e315fae888d2491cd4300e5a46772b0944
Arnav-Kadu <105575763+Arnav-Kadu@users.noreply.github.com>
2026-07-31 10:24:14 +0530

    Time: 4 ms (86.59%), Space: 25.6 MB (58.92%) - LeetHub

--- 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.cpp
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.cpp
@@ -1,27 +1,19 @@
 class Solution {
 public:
     int minimumPushes(string word) {
-        map<char,int>m;
-        for(auto i:word){
-            m[i]++;
-        }
-        vector<pair<int,char>>val;
-        for(auto i:m){
-            val.push_back({i.second,i.first});
-        }
-        sort(val.begin(),val.end(),[](const pair<int,int>&a,const pair<int,int>&b){
-            return a.first>b.first;
-        });
         int cnt[26]={0};
-        for(int i=0;i<val.size();i++){
-            cnt[val[i].second-'a']=i;
-        }
-        int ans=0;
         for(auto i:word){
-            ans+=(cnt[i-'a']/8 +1);
+            cnt[i-'a']++;
         }
+
+        sort(cnt,cnt+26,greater<int>());
+
+        int ans=0;
+        for(int i=0;i<26;i++){
+            ans+=cnt[i]*(i/8+1);
+        }
+
         return ans;
-
-
     }
 };
+
