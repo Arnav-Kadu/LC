a74918411ad8d0dcc47700ec60d44e2681cc8065
Arnav-Kadu <105575763+Arnav-Kadu@users.noreply.github.com>
2026-08-01 12:52:55 +0530

    Time: 25 ms (27.77%), Space: 9.6 MB (85.12%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -1,7 +1,5 @@
 class Solution {
 private:
-    map<tuple<int, int, int, int, int>, bool> dp;
-
     bool solve(int start, int end, vector<int>& nums, int a, int b, int turn) {
         if (start == end) {
             if (turn) {
@@ -12,22 +10,16 @@ private:
             return a >= b;
         }
 
-        auto state = make_tuple(start, end, a, b, turn);
-
-        if (dp.count(state)) {
-            return dp[state];
-        }
-
         if (turn) {
             bool first1 = solve(start + 1, end, nums, a + nums[start], b, !turn);
             bool last1 = solve(start, end - 1, nums, a + nums[end], b, !turn);
 
-            return dp[state] = first1 || last1;
+            return first1 || last1;
         } else {
             bool first2 = solve(start + 1, end, nums, a, b + nums[start], !turn);
             bool last2 = solve(start, end - 1, nums, a, b + nums[end], !turn);
 
-            return dp[state] = first2 && last2;
+            return first2 && last2;
         }
     }
 
