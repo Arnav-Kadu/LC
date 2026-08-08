dd2f33ac3988e5eb9c2b08099c7a13327dcc3bee
Arnav Kadu <105575763+Arnav-Kadu@users.noreply.github.com>
2026-08-08 17:57:59 +0530

    Time: 145 ms (13.57%), Space: 82.7 MB (8.57%) - LeetHub

---
+++ 2030-smallest-k-length-subsequence-with-occurrences-of-a-letter/2030-smallest-k-length-subsequence-with-occurrences-of-a-letter.cpp
@@ -0,0 +1,36 @@
+class Solution {
+  public:
+      string smallestSubsequence(string s, int k, char letter, int rep) {
+          int n = s.size();
+
+          vector<int> cnt(n + 1, 0);
+          for (int i = n - 1; i >= 0; --i) cnt[i] = cnt[i + 1] + (s[i] == letter);
+
+          vector<vector<int>> pos(26);
+          for (int i = 0; i < n; ++i) pos[s[i] - 'a'].push_back(i);
+          vector<int> ptr(26, 0);
+
+          string ans;
+          int need = rep, start = 0;
+
+          for (int j = 0; j < k; ++j) {
+              for (int ch = 0; ch < 26; ++ch) {
+                  while (ptr[ch] < (int)pos[ch].size() && pos[ch][ptr[ch]] < start) ptr[ch]++;
+                  if (ptr[ch] == (int)pos[ch].size()) continue;
+
+                  int i = pos[ch][ptr[ch]];
+                  bool isL = (ch + 'a' == letter);
+
+                  if (j + n - i < k) continue;
+                  if (cnt[i] < need) continue;
+                  if (need - isL > k - j - 1) continue;
+
+                  ans += char('a' + ch);
+                  need -= isL;
+                  start = i + 1;
+                  break;
+              }
+          }
+          return ans;
+      }
+  };
