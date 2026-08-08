56f3e2f00e8cc44540bc13657e8b87cc12c04a69
Arnav Kadu <105575763+Arnav-Kadu@users.noreply.github.com>
2026-08-08 18:01:42 +0530

    Time: 162 ms (9.28%), Space: 82.8 MB (8.57%) - LeetHub

--- 2030-smallest-k-length-subsequence-with-occurrences-of-a-letter/2030-smallest-k-length-subsequence-with-occurrences-of-a-letter.cpp
+++ 2030-smallest-k-length-subsequence-with-occurrences-of-a-letter/2030-smallest-k-length-subsequence-with-occurrences-of-a-letter.cpp
@@ -8,7 +8,7 @@ class Solution {
 
           vector<vector<int>> pos(26);
           for (int i = 0; i < n; ++i) pos[s[i] - 'a'].push_back(i);
-          vector<int> ptr(26, 0);
+          vector<int> ptr(26, 0); // which index to use for which character while seeing pos
 
           string ans;
           int need = rep, start = 0;
