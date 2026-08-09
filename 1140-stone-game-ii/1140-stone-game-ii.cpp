d04e1c5deadf9fec7def6b82a62148b27c137e69
Arnav Kadu <105575763+Arnav-Kadu@users.noreply.github.com>
2026-08-09 12:22:18 +0530

    Time: 15 ms (35.89%), Space: 15.4 MB (27.18%) - LeetHub

--- 1140-stone-game-ii/1140-stone-game-ii.cpp
+++ 1140-stone-game-ii/1140-stone-game-ii.cpp
@@ -1,48 +1,48 @@
 class Solution {
     int n;
+
 private:
-    int solve(int index,vector<int>&piles,vector<vector<vector<int>>>&dp,int move,int m){
-        if(index>=n){
+    int solve(int index, vector<int>& piles, vector<vector<vector<int>>>& dp, int move, int m) {
+        if(index >= n){
             return 0;
         }
 
-        if(dp[index][move][m]!=-1){
+        if(dp[index][move][m] != -1){
             return dp[index][move][m];
         }
 
-        if(index+2*m>=n){
-            int sum=0;
-            for(int i=index;i<n;i++){
-                sum+=piles[i];
+        int mx;
+
+        if(move){
+            mx = INT_MIN;
+            int sum = 0;
+
+            for(int i=1;i<=2*m && index+i<=n;i++){
+                sum += piles[index+i-1];
+
+                mx = max(mx, sum + solve(index+i,piles,dp,0,max(m,i)));
             }
-            return dp[index][move][m]=sum;
+        }
+        else{
+            mx = INT_MAX;
+
+            for(int i=1;i<=2*m && index+i<=n;i++){
+                mx = min(mx, solve(index+i,piles,dp,1,max(m,i)));
+            }
         }
 
-        int mx=INT_MIN;
-        int sum=0;
-
-        for(int i=1;i<=2*m && index+i<=n;i++){
-            sum+=piles[index+i-1];
-
-            mx=max(mx,sum-solve(index+i,piles,dp,!move,max(m,i)));
-        }
-
-        return dp[index][move][m]=mx;
+        return dp[index][move][m] = mx;
     }
 
 public:
     int stoneGameII(vector<int>& piles) {
-        n=piles.size();
+        n = piles.size();
 
-        vector<vector<vector<int>>>dp(n,vector<vector<int>>(2,vector<int>(n+1,-1)));
+        vector<vector<vector<int>>> dp(
+            n,
+            vector<vector<int>>(2,vector<int>(n+1,-1))
+        );
 
-        int total=0;
-        for(auto i:piles){
-            total+=i;
-        }
-
-        int diff=solve(0,piles,dp,1,1);
-
-        return (total+diff)/2;
+        return solve(0,piles,dp,1,1);
     }
 };
