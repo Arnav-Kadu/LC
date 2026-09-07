f22f302989fde7ce44d0dac8a1a122cd5b79cb51
Arnav Kadu <105575763+Arnav-Kadu@users.noreply.github.com>
2026-09-07 12:23:09 +0530

    Time: 0 ms (100%), Space: 9.4 MB (52.48%) - LeetHub

--- 0940-distinct-subsequences-ii/0940-distinct-subsequences-ii.cpp
+++ 0940-distinct-subsequences-ii/0940-distinct-subsequences-ii.cpp
@@ -4,15 +4,14 @@ public:
     int distinctSubseqII(string s) { 
         int n=s.size();
         vector<int>dp(n+1,0);
+        vector<int>see(26,-1);
         dp[0]=1;
         for(int i=1;i<=n;i++){
             dp[i]=(dp[i-1]*2)%mod;
-            for(int j=i-2;j>=0;j--){
-                if(s[i-1]==s[j]){
-                    dp[i]=(dp[i]-dp[j]+mod)%mod;
-                    break;
-                }
+            if(see[s[i-1]-'a']!=-1){
+              dp[i]=(dp[i]-dp[see[s[i-1]-'a']]+mod)%mod;
             }
+            see[s[i-1]-'a']=i-1;
         }
         return (dp[n]-1+mod)%mod;
     }
