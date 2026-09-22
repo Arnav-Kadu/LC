e6531031bc80378cb6dddd94973be6165c8c345c
Arnav Kadu <105575763+Arnav-Kadu@users.noreply.github.com>
2026-09-22 14:40:49 +0530

    Time: 453 ms (27.27%), Space: 300.5 MB (30.91%) - LeetHub

--- 3525-find-x-value-of-array-ii/3525-find-x-value-of-array-ii.cpp
+++ 3525-find-x-value-of-array-ii/3525-find-x-value-of-array-ii.cpp
@@ -2,13 +2,19 @@ class SegmentTree {
     struct Node {
         int prod;
         int dp[5];
+
+        Node() {
+            prod = 1;
+            for (int i = 0; i < 5; i++) dp[i] = 0;
+        }
     };
 
     int n, k;
     vector<Node> tree;
 
-    Node merge(const Node &a, const Node &b) {
-        Node res{};
+    Node merge(Node a, Node b) {
+        Node res;
+
         res.prod = 1LL * a.prod * b.prod % k;
 
         for (int r = 0; r < k; r++) {
@@ -19,75 +25,83 @@ class SegmentTree {
         return res;
     }
 
+    void build(int node, int l, int r, vector<int>& nums) {
+        if (l == r) {
+            int rem = nums[l] % k;
+
+            tree[node].prod = rem;
+            tree[node].dp[rem] = 1;
+
+            return;
+        }
+
+        int mid = (l + r) / 2;
+
+        build(node * 2, l, mid, nums);
+        build(node * 2 + 1, mid + 1, r, nums);
+
+        tree[node] = merge(tree[node * 2], tree[node * 2 + 1]);
+    }
+
+    void update(int node, int l, int r, int idx, int val) {
+        if (l == r) {
+            int rem = val % k;
+
+            tree[node] = Node();
+            tree[node].prod = rem;
+            tree[node].dp[rem] = 1;
+
+            return;
+        }
+
+        int mid = (l + r) / 2;
+
+        if (idx <= mid) {
+            update(node * 2, l, mid, idx, val);
+        } else {
+            update(node * 2 + 1, mid + 1, r, idx, val);
+        }
+
+        tree[node] = merge(tree[node * 2], tree[node * 2 + 1]);
+    }
+
+    Node query(int node, int l, int r, int ql, int qr) {
+        if (ql <= l && r <= qr) {
+            return tree[node];
+        }
+
+        int mid = (l + r) / 2;
+
+        if (qr <= mid) {
+            return query(node * 2, l, mid, ql, qr);
+        }
+
+        if (ql > mid) {
+            return query(node * 2 + 1, mid + 1, r, ql, qr);
+        }
+
+        Node left = query(node * 2, l, mid, ql, qr);
+        Node right = query(node * 2 + 1, mid + 1, r, ql, qr);
+
+        return merge(left, right);
+    }
+
 public:
     SegmentTree(vector<int>& nums, int k) {
+        n = nums.size();
         this->k = k;
 
-        n = 1;
-        while (n < nums.size()) n <<= 1;
+        tree.resize(4 * n);
 
-        tree.resize(2 * n);
-
-        for (int i = 0; i < n; i++) {
-            tree[i + n].prod = 1;
-            for (int r = 0; r < 5; r++) tree[i + n].dp[r] = 0;
-        }
-
-        for (int i = 0; i < nums.size(); i++) {
-            tree[n + i].prod = nums[i] % k;
-            tree[n + i].dp[nums[i] % k] = 1;
-        }
-
-        for (int i = n - 1; i >= 1; i--) {
-            tree[i] = merge(tree[i << 1], tree[i << 1 | 1]);
-        }
+        build(1, 0, n - 1, nums);
     }
 
     void update(int idx, int val) {
-        idx += n;
-
-        tree[idx].prod = val % k;
-
-        for (int r = 0; r < 5; r++) {
-            tree[idx].dp[r] = 0;
-        }
-
-        tree[idx].dp[val % k] = 1;
-
-        idx >>= 1;
-
-        while (idx) {
-            tree[idx] = merge(tree[idx << 1], tree[idx << 1 | 1]);
-            idx >>= 1;
-        }
+        update(1, 0, n - 1, idx, val);
     }
 
     Node query(int l, int r) {
-        l += n;
-        r += n + 1;
-
-        Node left{};
-        left.prod = 1;
-
-        Node right{};
-        right.prod = 1;
-
-        while (l < r) {
-            if (l & 1) {
-                left = merge(left, tree[l]);
-                l++;
-            }
-
-            if (r & 1) {
-                --r;
-                right = merge(tree[r], right);
-            }
-
-            l >>= 1;
-            r >>= 1;
-        }
-
-        return merge(left, right);
+        return query(1, 0, n - 1, l, r);
     }
 };
 
@@ -97,7 +111,6 @@ public:
         SegmentTree st(nums, k);
 
         vector<int> ans;
-        ans.reserve(queries.size());
 
         for (auto &q : queries) {
             int index = q[0];
